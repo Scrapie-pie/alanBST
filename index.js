c0d9e64636cfda2d8502a8865b9fb1095ab50afd
Sabrina Louge <neontec2@gmail.com>
2021-06-23 16:21:06 +0300

    Code refactor

--- index.js
+++ index.js
@@ -2,33 +2,29 @@
 
 
 class Node {
-	static id = 0;
+	static coreNode;
 
-	constructor(value = getRandom(-100, 100)) {
+		constructor(value = getRandom(-100, 100)) {
 		this.value = value;
 		this.left = null;
 		this.right = null;
 		this.root = null;
+		this.rootRef = null;
 		this.dom = Node.createNodeDom(this, this.value);
 		this.level = 1;
-		this.domId = Node.id;
+
+		Node.coreNode = Node.coreNode ?? this;
 	}
 
 	static createNodeDom(node, value) {
 		let domEl = createEl('div', 'binary-tree__node');
 		domEl.append(createEl('div', 'binary-tree__node-value', value.toString()));
+
 		domEl.addEventListener('click', function (e){
 			e.stopPropagation();
-			this.style.opacity = '0';
-			if (node.root.left === node) {
-				node.root.left = null
-			} else {
-				node.root.right = null
-			}
-			setTimeout(() => {
-				this.remove()
-			}, 500);
+			Node.remove(node, this)
 		});
+
 		domEl.addEventListener('mouseover', function (e){
 			e.stopPropagation();
 			this.classList.add('remove')
@@ -36,6 +32,7 @@ class Node {
 				child.classList.add('remove')
 			})
 		});
+
 		domEl.addEventListener('mouseout', function (e){
 			e.stopPropagation();
 			this.classList.remove('remove')
@@ -44,9 +41,6 @@ class Node {
 			})
 		});
 
-		domEl.setAttribute('id', `node_id-${Node.id++}`);
-		console.log(node)
-
 		return domEl;
 	}
 
@@ -54,8 +48,18 @@ class Node {
 		node.dom.classList.add('highlight')
 	}
 
-	static highlightRemove(node) {
-		node.dom.classList.add('remove')
+	static remove(node, dom) {
+		let parent = dom.parentNode;
+		let parentIsContainer = parent.classList.contains('binary-tree__node-container');
+		let duration = parseFloat(getComputedStyle(dom).animationDuration) * 1000;
+
+		dom.style.opacity = '0';
+
+		setTimeout(() => {
+			parentIsContainer ? parent.remove() : dom.remove();
+		}, duration);
+
+		node.root[node.rootRef] = null;
 	}
 
 	static insert(node, newNode) {
@@ -67,17 +71,20 @@ class Node {
 				newNode.level++;
 				return this.insert(node.right, newNode)
 			} else {
-				node.right = newNode
+				node.right = newNode;
+				newNode.rootRef = 'right';
 			}
 		} else if (newNode.value < node.value) {
 			if (node.left) {
 				newNode.level ++;
 				return this.insert(node.left, newNode)
 			} else {
-				node.left = newNode
+				node.left = newNode;
+				newNode.rootRef = 'left';
 			}
 		}
 		newNode.root = node;
+
 		return node
 	}
 }
@@ -106,70 +113,57 @@ function getOffset( el ) {
 	};
 }
 
-function drawLine(div1, div2, color, thickness) { // draw a line connecting elements
+function drawLine(to, div1, div2, color, thickness) {
 	let off1 = getOffset(div1);
 	let off2 = getOffset(div2);
-	// bottom right
 	let x1 = off1.left + off1.width / 2;
 	let y1 = off1.top + off1.height;
-	// top right
 	let x2 = off2.left + off2.width / 2;
-	let y2 = off2.top;
-	// distance
+	let y2 = off2.top + 1;
 	let length = Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
-	// center
 	let cx = ((x1 + x2) / 2) - (length / 2);
 	let cy = ((y1 + y2) / 2) - (thickness / 2);
-	// angle
 	let angle = Math.atan2((y1-y2),(x1-x2))*(180/Math.PI);
-	// make hr
 	let htmlLine = "<div style='padding:0px; margin:0px; height:" + thickness + "px; background-color:" + color + "; line-height:1px; position:absolute; left:" + cx + "px; top:" + cy + "px; width:" + length + "px; -moz-transform:rotate(" + angle + "deg); -webkit-transform:rotate(" + angle + "deg); -o-transform:rotate(" + angle + "deg); -ms-transform:rotate(" + angle + "deg); transform:rotate(" + angle + "deg);' />";
-	//
-	// alert(htmlLine);
-	document.getElementById('lines').innerHTML += htmlLine;
+
+	to.innerHTML += htmlLine;
 }
 
-console.log(document.innerHTML)
+function spaceBarHandler() {
+	if (!Node.coreNode) {
+		let rootNode = new Node();
+		rootNode.dom.addEventListener('click', () => Node.coreNode = null);
+		document.getElementById('tree').append(rootNode.dom);
 
-let tree = document.getElementById('tree');
-let rootNode;
+	} else {
+		let newNode = new Node;
+		let foundRootNode = Node.insert(Node.coreNode, newNode);
+
+		if (foundRootNode) {
+			let container = createEl('div', 'binary-tree__node-container');
+			foundRootNode.dom.append(container);
+			container.append(newNode.dom);
+
+			let direct = newNode.rootRef;
+
+			if (direct) {
+				let space = 300 / newNode.level;
+				container.style[direct] = `-${space}px`;
+				container.style.bottom = `-${newNode.level * 5 + 10}px`;
+
+				let lines = document.getElementById('lines')
+				drawLine(lines, foundRootNode.dom, newNode.dom, "black", 2);
+			}
+		}
+	}
+}
+
 
 document.addEventListener('keydown', event => {
 	if (event.code === 'Space') {
 		if (event.target === document.body) {
 			event.preventDefault();
 		}
-
-		if (!rootNode) {
-			rootNode = new Node();
-			rootNode.dom.addEventListener('click', () => rootNode = null);
-			tree.append(rootNode.dom);
-		} else {
-			let newNode = new Node;
-			let foundRootNode = Node.insert(rootNode, newNode);
-
-			if (foundRootNode) {
-				let container = createEl('div', 'binary-tree__node-container');
-				foundRootNode.dom.append(container);
-				container.append(newNode.dom);
-
-				let direct;
-
-				if (foundRootNode.right === newNode) {
-					direct = 'right';
-				} else if (foundRootNode.left === newNode) {
-					direct = 'left'
-				}
-
-				let space = 300 / newNode.level;
-				container.style[direct] = `-${space}px`;
-				container.style.bottom = `-${newNode.level * 5 + 10}px`;
-
-				let rootEl = document.getElementById(foundRootNode.dom.getAttribute('id'));
-				let childEl = document.getElementById(newNode.dom.getAttribute('id'));
-
-				drawLine(rootEl, childEl, "black", 2);
-			}
-		}
+		spaceBarHandler();
 	}
 });
